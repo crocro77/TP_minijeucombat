8555b84ea8c4643185d5dae1d4d8938c88ab9a18
ntonyyy <anthony.fumo@gmail.com>
2018-10-17 23:10:32 +0200

    autoload, instance du PDO, instance du manager dans le html

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+// On enregistre notre autoload.
+function chargerClasse($classname)
+{
+    require $classname . '.php';
+}
+
+spl_autoload_register('chargerClasse');
+
+$db = new PDO('mysql:host=localhost;dbname=combats', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
+
+$manager = new PersonnagesManager($db);
+
+if (isset($_POST['creer']) && isset($_POST['nom'])) // Si on a voulu créer un personnage.
+{
+    $perso = new Personnage(['nom' => $_POST['nom']]); // On crée un nouveau personnage.
+
+    if (!$perso->nomValide()) {
+        $message = 'Le nom choisi est invalide.';
+        unset($perso);
+    } elseif ($manager->exists($perso->nom())) {
+        $message = 'Le nom du personnage est déjà pris.';
+        unset($perso);
+    } else {
+        $manager->add($perso);
+    }
+} elseif (isset($_POST['utiliser']) && isset($_POST['nom'])) // Si on a voulu utiliser un personnage.
+{
+    if ($manager->exists($_POST['nom'])) // Si celui-ci existe.
+    {
+        $perso = $manager->get($_POST['nom']);
+    } else {
+        $message = 'Ce personnage n\'existe pas !'; // S'il n'existe pas, on affichera ce message.
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,6 +44,10 @@
     <meta charset="utf-8" />
   </head>
   <body>
+<?php
+if (isset($message)) // On a un message à afficher ?
+echo '<p>', $message, '</p>'; // Si oui, on l'affiche.
+?>
     <form action="" method="post">
       <p>
         Nom : <input type="text" name="nom" maxlength="50" />
