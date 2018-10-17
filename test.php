6537928aa3ea2f4b2416e1b25a371ff04ca0d732
ntonyyy <anthony.fumo@gmail.com>
2018-10-17 17:27:53 +0200

    hydratation

--- test.php
+++ test.php
@@ -1,31 +1,63 @@
 <?php
 class Personnage
 {
-  private $_id,
-          $_degats,
+  private $_degats,
+          $_id,
           $_nom;
   
-  const CEST_MOI = 1;
-  const PERSONNAGE_TUE = 2;
-  const PERSONNAGE_FRAPPE = 3;
+  const CEST_MOI = 1; // Constante renvoyée par la méthode `frapper` si on se frappe soi-même.
+  const PERSONNAGE_TUE = 2; // Constante renvoyée par la méthode `frapper` si on a tué le personnage en le frappant.
+  const PERSONNAGE_FRAPPE = 3; // Constante renvoyée par la méthode `frapper` si on a bien frappé le personnage.
+  
+  
+  public function __construct(array $donnees)
+  {
+    $this->hydrate($donnees);
+  }
   
   public function frapper(Personnage $perso)
   {
-    // Avant tout : vérifier qu'on ne se frappe pas soi-même.
-      // Si c'est le cas, on stoppe tout en renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
+    if ($perso->id() == $this->_id)
+    {
+      return self::CEST_MOI;
+    }
     
-    // On indique au personnage frappé qu'il doit recevoir des dégâts.
+    // On indique au personnage qu'il doit recevoir des dégâts.
+    // Puis on retourne la valeur renvoyée par la méthode : self::PERSONNAGE_TUE ou self::PERSONNAGE_FRAPPE
+    return $perso->recevoirDegats();
+  }
+  
+  public function hydrate(array $donnees)
+  {
+    foreach ($donnees as $key => $value)
+    {
+      $method = 'set'.ucfirst($key);
+      
+      if (method_exists($this, $method))
+      {
+        $this->$method($value);
+      }
+    }
   }
   
   public function recevoirDegats()
   {
-    // On augmente de 5 les dégâts.
+    $this->_degats += 5;
     
-    // Si on a 100 de dégâts ou plus, la méthode renverra une valeur signifiant que le personnage a été tué.
+    // Si on a 100 de dégâts ou plus, on dit que le personnage a été tué.
+    if ($this->_degats >= 100)
+    {
+      return self::PERSONNAGE_TUE;
+    }
     
-    // Sinon, elle renverra une valeur signifiant que le personnage a bien été frappé.
+    // Sinon, on se contente de dire que le personnage a bien été frappé.
+    return self::PERSONNAGE_FRAPPE;
   }
   
+  
+  // GETTERS //
+  
+
   public function degats()
   {
     return $this->_degats;
