156cc9569952f5a3e77ca3e3458c3144c0200135
ntonyyy <anthony.fumo@gmail.com>
2018-10-17 17:13:35 +0200

    ajout des constantes

--- test.php
+++ test.php
@@ -5,14 +5,18 @@ class Personnage
           $_degats,
           $_nom;
   
+  const CEST_MOI = 1;
+  const PERSONNAGE_TUE = 2;
+  const PERSONNAGE_FRAPPE = 3;
+  
   public function frapper(Personnage $perso)
   {
     // Avant tout : vérifier qu'on ne se frappe pas soi-même.
-    // Si c'est le cas, on stoppe tout en renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
-
+      // Si c'est le cas, on stoppe tout en renvoyant une valeur signifiant que le personnage ciblé est le personnage qui attaque.
+    
     // On indique au personnage frappé qu'il doit recevoir des dégâts.
   }
-
+  
   public function recevoirDegats()
   {
     // On augmente de 5 les dégâts.
