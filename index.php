cd3130a83e666499575069bb138195b166d3f768
ntonyyy <anthony.fumo@gmail.com>
2018-10-18 21:48:44 +0200

    personnages à frapper

--- index.php
+++ index.php
@@ -16,15 +16,15 @@ if (isset($_GET['deconnexion']))
   exit();
 }
 
-if (isset($_SESSION['perso'])) // Si la session perso existe, on restaure l'objet.
-{
-  $perso = $_SESSION['perso'];
-}
-
 $db = new PDO('mysql:host=localhost;dbname=combats', 'root', '');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
 
 $manager = new PersonnagesManager($db);
+
+if (isset($_SESSION['perso'])) // Si la session perso existe, on restaure l'objet.
+{
+  $perso = $_SESSION['perso'];
+}
 
 if (isset($_POST['creer']) && isset($_POST['nom'])) // Si on a voulu créer un personnage.
 {
@@ -56,6 +56,52 @@ elseif (isset($_POST['utiliser']) && isset($_POST['nom'])) // Si on a voulu util
     {
         $message = 'Ce personnage n\'existe pas !'; // S'il n'existe pas, on affichera ce message.
     }
+}
+
+elseif (isset($_GET['frapper'])) // Si on a cliqué sur un personnage pour le frapper.
+{
+  if (!isset($perso))
+  {
+    $message = 'Merci de créer un personnage ou de vous identifer.';
+  }
+
+  else
+  {
+    if (!manager->exists((int) $_GET['frapper ']))
+    {
+      $message = 'Le personnage que vous voulez frapper n\'existe pas !';
+    }
+
+    else
+    {
+      $persoAFrapper = $manager->get((int) $_GET['frapper']);
+
+      $retour = $perso->frapper($persoAFrapper); // On stocke dans $retour les éventuelles erreurs ou messages que renvoie la méthode frapper.
+
+      switch ($retour)
+      {
+        case Personnage::CEST_MOI :
+          $message = 'Mais... pourquoi voulez-vous vous frapper ???';
+          break;
+
+        case Personnage::PERSONNAGE_FRAPPE :
+          $message = 'Le personnage a bien été frappé !';
+
+          $manager->update($perso);
+          $manager->update($persoAFrapper);
+
+          break;
+
+        case Personnage::PERSONNAGE_TUE :
+          $message = 'Vous avez tué ce personnage !';
+
+          $manager->update($perso);
+          $manager->delete($persoAFrapper);
+
+        break;
+      }
+    }
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -100,7 +146,9 @@ if (empty($persos))
 else
 {
   foreach ($persos as $unPerso)
+  {
     echo '<a href="?frapper=', $unPerso->id(), '">', htmlspecialchars($unPerso->nom()), '</a> (dégâts : ', $unPerso->degats(), ')<br />';
+  }
 }
 ?>
       </p>
